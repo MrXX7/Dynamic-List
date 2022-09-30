f9a6e00639b9bf235506b0fa7ee4476bff7d36c1
Oncu <oohancan@gmail.com>
2022-09-30 15:02:47 +0300

    Initial Commit

---
+++ Dynamic List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dynamic List
+//
+//  Created by Oncu Can on 30.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dynamic List/Dynamic_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dynamic_ListApp.swift
+//  Dynamic List
+//
+//  Created by Oncu Can on 30.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Dynamic_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
